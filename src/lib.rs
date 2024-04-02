8193c66cee0bdba2b2f8fe3de1b9ae16c0292cc5
David O <148862921+David001d@users.noreply.github.com>
2024-04-03 01:12:35 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
-pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {message}")
+ pub fn reply(message: &str) -> &str {
+  if message.is_empty() {
+    "Fine. Be that way!"
+  } else if message.is_uppercase() {
+    if message.ends_with("?") {
+      "Calm down, I know what I'm doing!"
+    } else {
+      "Whoa, chill out!"
+    }
+  } else if message.ends_with("?") {
+    "Sure."
+  } else {
+    "Whatever."
+  }
 }
